f841be896d484b3cd2564658cdd3062d0ac3103a
Maliha Tabassum Hridila <malihatabassumhridila@gmail.com>
2026-07-11 05:54:23 -0700

    Refactor booking_history.php HTML structure

--- booking_history.php
+++ booking_history.php
@@ -323,7 +323,7 @@
                     </thead>
                     <tbody>
                     <?php
-                        $conn = new mysqli("localhost", "root", "hridila44", "iwp");
+                        $conn = new mysqli("localhost", "root", "", "hotel_management_system");
                         if($conn->connect_error) {
                             die("Connection failed: " . $conn->connect_error);
                         }
