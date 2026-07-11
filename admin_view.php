92a8e170f1e6d2fb12d34d0a2155f290698578fa
Maliha Tabassum Hridila <malihatabassumhridila@gmail.com>
2026-07-11 05:53:50 -0700

    Fix HTML entities and update database connection

--- admin_view.php
+++ admin_view.php
@@ -234,7 +234,7 @@
                 <h2 class="table-title">Rooms Info</h2>
 
                 <?php
-                    $conn = new mysqli("localhost", "root", "hridila44", "iwp");
+                    $conn = new mysqli("localhost", "root", "", "hotel_management_system");
                     if($conn->connect_error) {
                         die("Connection failed: " . $conn->connect_error);
                     }
